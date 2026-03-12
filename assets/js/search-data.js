05b80931d4d7a6e04fa63ffd570585f09368b6b6
yoon-group <149346748+yoon-group@users.noreply.github.com>
2026-03-12 15:18:26 +0000

    Deploying to gh-pages from @ yoon-group/yoon-group.github.io@34060f46788eeaa4f310350abceeb4a41df73ec1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -249,7 +249,7 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "teachings-hydrogeology-field-camp",
           title: 'Hydrogeology Field Camp',
-          description: "This 2-credit intensive field- and lab-based course introduces students to groundwater flow, karst hydrogeology, and surface water-groundwater interactions through classroom instruction, laboratory exercises, and multi-day field investigations. Students will collect, analyze, and synthesize hydrogeologic and hydrochemical data into professional technical reports.",
+          description: "This 2-credit intensive field- and lab-based course introduces students to groundwater flow, karst hydrogeology, and surface water-groundwater interactions through classroom instruction, laboratory exercises, and multi-day field investigations. Students will collect, analyze, and synthesize hydrogeologic and hydrochemical data into professional technical reports. These concepts and practical skills are directly relevant to careers in environmental and consulting work and help prepare students for the Professional Geologist (PG) licensure exam.",
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/hydrocamp/";
             },},{id: "teachings-hydrogeologic-modeling",
